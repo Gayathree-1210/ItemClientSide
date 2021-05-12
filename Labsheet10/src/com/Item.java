d81cede8377c3c3e04a82dd9f2b6f7a1f67d7a91
Gayathree Rajakaruna <gy3.indunil@gmail.com>
2021-05-12 21:41:16 +0530

    This is Lab sheet 10

---
+++ Labsheet10/src/com/Item.java
@@ -0,0 +1,193 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Item {
+	public Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/test","root","");
+			
+			//for testing
+			System.out.println("Suucessfully connected");
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+		
+	}
+	//Insert
+		public String insertItem(String code, String name, String price, String desc)
+		{
+			String output = "";
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database";
+				}
+
+				// create a prepared statement
+				String query = " insert into item(`itemID`,`itemCode`,`itemName`,`itemPrice`,`itemDesc`) values (?, ?, ?, ?, ?)";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+
+				// binding values
+				preparedStmt.setInt(1, 0);
+				preparedStmt.setString(2, code);
+				preparedStmt.setString(3, name);
+				preparedStmt.setDouble(4, Double.parseDouble(price));
+				preparedStmt.setString(5, desc); 
+
+				//execute the statement
+				preparedStmt.execute();
+				con.close();
+
+				String newItems = readItems();
+				output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+				
+			}
+			catch (Exception e)
+			{
+				output = "{\"status\":\"error\", \"data\":\"Error while inserting the item.\"}";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+
+		//Read
+		public String readItems()
+		{
+			String output = "";
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database for reading.";
+				}
+				// Prepare the HTML table to be displayed
+				output = "<table  class='table table-dark table-striped'><tr><th>Item Code</th>"
+						+"<th>Item Name</th><th>Item Price</th>"
+						+ "<th>Item Description</th>"
+						+ "<th>Update</th><th>Remove</th></tr>";
+
+				String query = "select * from item";
+				Statement stmt = con.createStatement();
+				ResultSet rs = stmt.executeQuery(query);
+
+				// iterate through the rows in the result set
+				while (rs.next())
+				{
+					String itemID = Integer.toString(rs.getInt("itemID"));
+					String itemCode = rs.getString("itemCode");
+					String itemName = rs.getString("itemName");
+					String itemPrice = Double.toString(rs.getDouble("itemPrice"));
+					String itemDesc = rs.getString("itemDesc");
+
+					// Add a row into the HTML table
+					output += "<tr><td>" + itemCode + "</td>";
+					output += "<td>" + itemName + "</td>";
+					output += "<td>" + itemPrice + "</td>"; 
+					output += "<td>" + itemDesc + "</td>";
+					
+
+					// buttons
+					output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary' data-itemid='" + itemID + "'></td>"
+							+"<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-itemid='" + itemID + "'>" + "</td></tr>";
+				}
+
+				con.close();
+
+				// Complete the HTML table
+				output += "</table>";
+			}
+			catch (Exception e)
+			{
+				output = "Error while reading the items.";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+
+		//Update
+		public String updateItem(int id, String code, String name, String price, String desc)
+		{
+			String output = "";
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database";
+				}
+
+				// create a prepared statement
+				String query = "update item set `itemCode`=?,`itemName`=?,`itemPrice`=?,`itemDesc`=? where `itemID`=?";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+
+				// binding values
+				preparedStmt.setString(1, code);
+				preparedStmt.setString(2, name);
+				preparedStmt.setDouble(3, Double.parseDouble(price));
+				preparedStmt.setString(4, desc);
+				preparedStmt.setInt(5, id);
+
+				//execute the statement
+				preparedStmt.executeUpdate();
+				con.close();
+
+				String newItems = readItems();
+				output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+				
+				
+			}
+			catch (Exception e)
+			{
+				output = "{\"status\":\"error\", \"data\":\"Error while updating the item.\"}";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+
+		//Delete
+		public String removeItem(int id)
+		{
+			String output = "";
+			try
+			{
+				Connection con = connect();
+				if (con == null)
+				{
+					return "Error while connecting to the database";
+				}
+
+				// create a prepared statement
+				String query = "delete from item where `itemID`=?;";
+				PreparedStatement preparedStmt = con.prepareStatement(query);
+
+				// binding values
+				preparedStmt.setInt(1, id);
+
+				//execute the statement
+				preparedStmt.executeUpdate();
+				con.close();
+
+				String newItems = readItems();
+				output = "{\"status\":\"success\", \"data\": \"" + newItems + "\"}";
+			}
+			catch (Exception e)
+			{
+				output = "{\"status\":\"error\", \"data\":\"Error while deleting the item.\"}";
+				System.err.println(e.getMessage());
+			}
+			return output;
+		}
+
+		
+	}
